64d7967fbc72dd20d945034ac6ec49d9074900d6
MelSovereign <mattpoindexter@gmail.com>
2011-10-30 23:07:04 -0400

    added recursive gcd

---
+++ oct_24_2011/recursion_gcd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <cassert>//for assert
+#include <algorithm>//for min
+using namespace std;
+
+int recursion_gcd(int a, int b, int test_number);
+
+int main()
+{
+  cout << "The gcd of 30 and 105 is " << recursion_gcd(30, 105, 30) << ".\n";
+  return EXIT_SUCCESS;
+}
+
+int recursion_gcd(int a, int b, int test_number = -1)
+{
+	if (test_number == -1) {
+		test_number = min(a, b);
+	}
+	assert(a >= 0 && b >= 0);
+	if (a % test_number == 0 && b % test_number == 0){
+	return test_number;
+	}
+	return recursion_gcd(a, b, --test_number);
+}
