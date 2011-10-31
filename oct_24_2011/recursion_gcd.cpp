146dee8b618b3e53280f1f5343c75375161c29d3
MelSovereign <mattpoindexter@gmail.com>
2011-10-31 00:12:46 -0400

    revision of recursion gcd

--- oct_24_2011/recursion_gcd.cpp
+++ oct_24_2011/recursion_gcd.cpp
@@ -8,18 +8,15 @@ int recursion_gcd(int a, int b, int test_number);
 
 int main()
 {
-  cout << "The gcd of 30 and 105 is " << recursion_gcd(30, 105, 30) << ".\n";
+  cout << "The gcd of 30 and 105 is " << recursion_gcd(30, 105, min(30, 105)) << ".\n";
   return EXIT_SUCCESS;
 }
 
-int recursion_gcd(int a, int b, int test_number = -1)
+int recursion_gcd(int a, int b, int test_number)
 {
-	if (test_number == -1) {
-		test_number = min(a, b);
-	}
 	assert(a >= 0 && b >= 0);
 	if (a % test_number == 0 && b % test_number == 0){
-	return test_number;
+		return test_number;
 	}
 	return recursion_gcd(a, b, --test_number);
 }
