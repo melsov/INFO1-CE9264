13df28c43d0d4cef05bc195ac0c83dce106f0e4e
MelSovereign <mattpoindexter@gmail.com>
2011-11-13 13:44:50 -0500

    added stack.cpp and stack.h (hw 2.6 a and b)

---
+++ nov_7_2011/stack_proj/stack.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include "stack.h"
+using namespace std;
+
+stack::stack(const stack& another)
+{
+	for (size_t i =0; i < stack_max_size; ++i) {
+		a[i] = another.a[i];
+	}
+	
+	p = a;
+}
+
+stack::~stack()   //destructor
+{
+	//cout << "destructor for stack\n";
+	
+	if (size() != 0) {
+		cerr << "Warning: stack still contains " << size() << " value(s).\n";
+	}
+}
+
+//Push a value onto the stack.
+
+void stack::push(int i)
+{
+	if (full()) {   //overflow
+		cerr << "Can't push when size " << size() << " == capacity "
+		<< stack_max_size << ".\n";
+		exit(EXIT_FAILURE);
+	}
+
+	*p++ = i; 
+}
+
+//Pop a value off the stack.
+
+int stack::pop()
+{
+	if (empty()) {                //underflow
+		cerr << "Can't pop when size " << size() << " == 0.\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	return *--p;
+}
---
+++ nov_7_2011/stack_proj/stack.h
@@ -0,0 +1,21 @@
+#ifndef STACKH       //C++ example
+#define STACKH
+#include <cstddef>   //for size_t
+
+const size_t stack_max_size = 100;
+
+class stack {
+	int a[stack_max_size];
+	int* p;
+public:
+	stack() {p = a;}   //constructor: start with the stack empty
+	stack(const stack& another);
+	~stack();          //destructor
+	
+	void push(int i);
+	int pop();         //C++ doesn't need the keyword void
+	bool empty() const {return p == a;}  
+	bool full() const {return p == &a[stack_max_size];}
+	size_t size() const {return p - a;}
+};
+#endif
